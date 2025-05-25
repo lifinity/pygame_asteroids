cfc884cfec87202dd07a93c9f1de4fabd937c9a8
lifinity <ll694@cornell.edu>
2025-05-25 15:35:56 -0400

    + player movement & sprite grouping

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+
         screen.fill(COLOR_BLACK)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        dt = clock.tick(FPS_LIMIT) / 1000
 
 if __name__ == "__main__":
     main()
