8cfcd3bfa46187363d7fb4591d40343044f00214
lifinity <ll694@cornell.edu>
2025-05-25 18:23:10 -0400

    + player-asteroid collision detection

--- circleshape.py
+++ circleshape.py
@@ -19,3 +19,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+    
+    def collides(self, obj):
+        return self.position.distance_to(obj.position) <= self.radius + obj.radius
